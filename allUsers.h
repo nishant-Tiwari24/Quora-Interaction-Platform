aed9552b942b874ca19b571a92ea1b3799db85f6
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-11-22 22:00:10 +0530

    Mutual friends and Searching Feature

---
+++ allUsers.h
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class SearchingPanel
+{
+public:
+    unordered_map<int, vector<string>> Users;
+
+    int returnAscii(const string &str)
+    {
+        int sum = 0;
+        for (char ch : str)
+        {
+            sum += static_cast<int>(ch);
+        }
+        return (sum % 10);
+    }
+
+    unordered_map<int, vector<string>> detailsToHashTable()
+    {
+        ifstream read("login.txt");
+        if (!read.is_open())
+        {
+            cerr << "Error opening file\n";
+            return Users;
+        }
+        while (!read.eof())
+        {
+            string username;
+            int a;
+            getline(read, username);
+            a = returnAscii(username);
+            Users[a].insert(Users[a].end(), username);
+            getline(read, username);
+            getline(read, username);
+        }
+        read.close();
+        return Users;
+    }
+
+    bool searchUser(const string &username)
+    {
+        int key = returnAscii(username);
+        auto it = Users.find(key);
+
+        if (it != Users.end())
+        {
+            const vector<string> &usernames = it->second;
+            for (const string &user : usernames)
+            {
+                if (user == username)
+                {
+                    return true;
+                }
+            }
+        }
+        return false; // Added return statement
+    }
+};
